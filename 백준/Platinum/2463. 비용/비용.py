21eb0eb6ef13b77d7e61e9d08d9cdaf4f24b9e52
Gaeunyee <162317314+Gaeunyee@users.noreply.github.com>
2025-01-23 17:29:35 +0900

    [Platinum V] Title: 비용, Time: 608 ms, Memory: 129472 KB -BaekjoonHub

---
+++ 백준/Platinum/2463. 비용/비용.py
@@ -0,0 +1,48 @@
+import sys
+from heapq import *
+m = 10**9
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+hq = []
+s = 0
+for _ in range(M):
+    u, v, w = map(int, input().split())
+    s += w
+    heappush(hq, (-w, u, v))
+res = 0
+par = [i for i in range(N+1)]
+size = [1]*(N+1)
+cost = 0
+
+def find(a):
+    if a == par[a]:
+        return a
+    par[a] = find(par[a])
+    return par[a]
+
+
+def union(a, b, w):
+    global res, cost
+    x, y = find(a), find(b)
+    if x == y:
+        cost += -w
+        return False
+    res = (res+(s-cost) * size[x] * size[y])%m
+    cost += -w
+
+    if x > y:
+        par[x] = y
+        size[y] += size[x]
+    else:
+        par[y] = x
+        size[x] += size[y]
+    return True
+
+
+while hq:
+    w, u, v = heappop(hq)
+    union(u, v, w)
+
+print(res)
+
