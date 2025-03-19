cab6f7eb32ddb313dcc8cd0d5e9446310af061f4
Gaeunyee <162317314+Gaeunyee@users.noreply.github.com>
2025-03-19 12:17:40 +0900

    [Silver IV] Title: 수열 정렬, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1015. 수열 정렬/수열 정렬.py
+++ 백준/Silver/1015. 수열 정렬/수열 정렬.py
@@ -4,11 +4,11 @@ input = sys.stdin.readline
 
 N = int(input().strip())
 arr = list(map(int, input().split()))
-z = [(i, arr[i]) for i in range(N)]
-z.sort(key=lambda x: x[1])
+z = [(arr[i], i) for i in range(N)]
+z.sort()
 
-r = [(j, z[j][0]) for j in range(N)]
-r.sort(key=lambda x: x[1])
+r = [(z[j][1], j) for j in range(N)]
+r.sort()
 
 for i in range(N):
-    print(r[i][0], end=' ')
+    print(r[i][1], end=' ')
