08f89547fd8de534a9bbd03821e006ff447e3c95
Gaeunyee <162317314+Gaeunyee@users.noreply.github.com>
2025-03-30 01:41:05 +0900

    [Silver IV] Title: 수 찾기, Time: 332 ms, Memory: 135764 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,27 +1,30 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input().strip())
-lst = list(map(int, input().split()))
-lst.sort()
 
-M = int(input().strip())
-target = list(map(int, input().split()))
-query = []
-for i in range(M):
-    query.append((target[i], i))
+def solve():
+    N = int(input().strip())
+    lst = list(map(int, input().split()))
+    lst.sort()
 
-query.sort()
-res = [0]*M
-idx = 0
-for n, i in query:
-    while n > lst[idx] and idx < N-1:
-        idx += 1
-    if lst[idx] == n:
-        res[i] = 1
-    else:
-        res[i] = 0
-for i in res:
-    print(i)
+    M = int(input().strip())
+    query = []
+    i = 0
+    for n in map(int, input().split()):
+        query.append((n, i))
+        i += 1
 
+    query.sort()
+    res = [0]*M
+    idx = 0
+    for n, i in query:
+        while n > lst[idx] and idx < N-1:
+            idx += 1
+        if lst[idx] == n:
+            res[i] = 1
+        else:
+            res[i] = 0
+    for i in res:
+        print(i)
 
+solve()
