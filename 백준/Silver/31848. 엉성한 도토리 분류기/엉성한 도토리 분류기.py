c9c3ccbc47a2e393e82240d7ad9c7cfa5e45736e
Gaeunyee <162317314+Gaeunyee@users.noreply.github.com>
2025-04-05 01:25:34 +0900

    [Silver II] Title: 엉성한 도토리 분류기, Time: 192 ms, Memory: 134876 KB -BaekjoonHub

--- 백준/Silver/31848. 엉성한 도토리 분류기/엉성한 도토리 분류기.py
+++ 백준/Silver/31848. 엉성한 도토리 분류기/엉성한 도토리 분류기.py
@@ -1,23 +1,24 @@
 import sys
-import heapq
 input = sys.stdin.readline
 
-N = int(input().strip())
-hole = list(map(int, input().split()))
-for i in range(N):
-    hole[i] += i
-Q = int(input().strip())
-hq = []
-dot = list(map(int, input().split()))
-dotori = []
-for i in range(Q):
-    dotori.append((i, dot[i]))
-dotori.sort(key=lambda x: x[1])
-
-idx = 0
-for i in range(Q):
-    while dotori[i][1] > hole[idx]:
+def solve():
+    N = int(input().strip())
+    hole = [(-1, 0)]
+    idx = 0
+    for size in map(int, input().split()):
+        if hole[-1][0] < size+idx:
+            hole.append((size+idx, idx+1))
         idx += 1
-    heapq.heappush(hq, (dotori[i][0], idx+1))
-while hq:
-    print(heapq.heappop(hq)[1], end=' ')
+
+    Q = int(input().strip())
+    for i in map(int, input().split()):
+        l, r = 0, len(hole)-1
+        while l+1 < r:
+            m = (l+r)//2
+            if hole[m][0] >= i:
+                r = m
+            else:
+                l = m
+        print(hole[r][1])
+
+solve()
