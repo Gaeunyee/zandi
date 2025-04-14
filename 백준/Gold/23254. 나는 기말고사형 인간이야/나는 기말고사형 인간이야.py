e4816eeffecacdf93e24bcd8d295d9bcf3bac41c
Gaeunyee <162317314+Gaeunyee@users.noreply.github.com>
2025-04-15 02:22:47 +0900

    [Gold V] Title: 나는 기말고사형 인간이야, Time: 540 ms, Memory: 169484 KB -BaekjoonHub

--- 백준/Gold/23254. 나는 기말고사형 인간이야/나는 기말고사형 인간이야.py
+++ 백준/Gold/23254. 나는 기말고사형 인간이야/나는 기말고사형 인간이야.py
@@ -9,13 +9,17 @@ ef = list(map(int, input().split()))
 hq = []
 for i in range(M):
     heappush(hq, (-min(100-score[i], ef[i]), i))
+N *= 24
+while N:
+    d, idx = heappop(hq)
+    if d == 0:
+        break
+    r = 100-score[idx]
+    k = min(N, r//(-d))
+    score[idx] += k*(-d)
+    N -= k
 
-for i in range(N*24):
-    d, idx = hq[0]
-    score[idx] -= d
-    if 100-score[idx] < -d:
-        heappop(hq)
-        heappush(hq, (-(100-score[idx]), idx))
+    heappush(hq, (-(100-score[idx]), idx))
 
 print(sum(score))
 
