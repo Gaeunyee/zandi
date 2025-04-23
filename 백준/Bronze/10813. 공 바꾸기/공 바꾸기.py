1367c1b3d69b45551a8da12ec325d5bd71183a04
Gaeunyee <162317314+Gaeunyee@users.noreply.github.com>
2025-04-23 22:28:27 +0900

    [Bronze II] Title: 공 바꾸기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+arr = [i for i in range(N+1)]
+
+for _ in range(M):
+    i, j = map(int, input().split())
+    arr[i], arr[j] = arr[j], arr[i]
+
+print(*arr[1:])
