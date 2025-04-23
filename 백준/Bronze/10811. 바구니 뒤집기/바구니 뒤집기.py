f21755a306b1206f0b90771953613ea6a36b3e2e
Gaeunyee <162317314+Gaeunyee@users.noreply.github.com>
2025-04-23 22:29:42 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+arr = [i for i in range(N+1)]
+
+for _ in range(M):
+    i, j = map(int, input().split())
+    arr[i:j+1] = arr[j:i-1:-1]
+
+print(*arr[1:])
