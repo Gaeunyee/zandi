69793d836dd53a62c2666096bceac5379ed2936b
Gaeunyee <162317314+Gaeunyee@users.noreply.github.com>
2025-04-29 00:48:50 +0900

    [Gold II] Title: 미확인 도착지, Time: 220 ms, Memory: 120352 KB -BaekjoonHub

--- 백준/Gold/9370. 미확인 도착지/미확인 도착지.py
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.py
@@ -4,19 +4,18 @@ input = sys.stdin.readline
 INF = 10**9
 
 T = int(input().strip())
-
-for _ in range(T):
+def solve():
     n, m, t = map(int, input().split())
     s, g, h = map(int, input().split())
-    graph = [[] for _ in range(n+1)]
+    graph = [[] for _ in range(n + 1)]
     for _ in range(m):
         a, b, d = map(int, input().split())
         graph[a].append((b, d))
         graph[b].append((a, d))
 
-    table = [INF]*(n+1)
+    table = [INF] * (n + 1)
     table[s] = 0
-    used = [0]*(n+1)
+    used = [0] * (n + 1)
     qu = []
     heappush(qu, (0, s))
     while qu:
@@ -37,9 +36,11 @@ for _ in range(T):
 
     for _ in range(t):
         used[int(input().strip())] *= -1
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         if used[i] < 0:
             print(i, end=' ')
     print()
+for _ in range(T):
+    solve()
 
 
