db416f7f4fdd9fe1af35036ed01ab5a69c2d6128
Gaeunyee <162317314+Gaeunyee@users.noreply.github.com>
2025-05-17 00:49:48 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 544 ms, Memory: 360292 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,28 @@
+import sys
+sys.setrecursionlimit(10**5+7)
+input = sys.stdin.readline
+
+
+N, M, R = map(int, input().split())
+visited = [0]*(N+1)
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+counter = 0
+
+
+def dfs(node):
+    global counter
+    counter += 1
+    visited[node] = counter
+    for nxt in sorted(graph[node], reverse=True):
+        if visited[nxt] == 0:
+            dfs(nxt)
+
+
+dfs(R)
+for i in visited[1:]:
+    print(i)
