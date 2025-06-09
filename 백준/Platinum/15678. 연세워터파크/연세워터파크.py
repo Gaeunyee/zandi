ce1d65dffcd3b5b9eb078e0633df58299cac0bb6
Gaeunyee <162317314+Gaeunyee@users.noreply.github.com>
2025-06-09 23:58:35 +0900

    [Platinum V] Title: 연세워터파크, Time: 120 ms, Memory: 45604 KB -BaekjoonHub

--- 백준/Platinum/15678. 연세워터파크/연세워터파크.py
+++ 백준/Platinum/15678. 연세워터파크/연세워터파크.py
@@ -2,21 +2,21 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
+def solve():
+    N, D = map(int, input().split())
+    dp = list(map(int, input().split()))
 
-N, D = map(int, input().split())
-dp = list(map(int, input().split()))
+    qu = deque()
+    qu.append(0)
+    for i in range(1, N):
+        while qu[0] < i-D:
+            qu.popleft()
+        dp[i] += dp[qu[0]] if dp[qu[0]] > 0 else 0
+        while qu and dp[qu[-1]] < dp[i]:
+            qu.pop()
+        qu.append(i)
 
-qu = deque()
-qu.append(0)
-for i in range(1, N):
-    while qu[0] < i-D:
-        qu.popleft()
-    dp[i] += dp[qu[0]] if dp[qu[0]] > 0 else 0
-    while qu and dp[qu[-1]] < dp[i]:
-        qu.pop()
-    qu.append(i)
+    print(max(dp))
 
-print(max(dp))
-
-
+solve()
 
