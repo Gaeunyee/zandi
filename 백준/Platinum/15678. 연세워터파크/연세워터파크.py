58c504ec48f62d4c99e2b7b0cd1814ae02012fa1
Gaeunyee <162317314+Gaeunyee@users.noreply.github.com>
2025-06-09 23:57:15 +0900

    [Platinum V] Title: 연세워터파크, Time: 180 ms, Memory: 44920 KB -BaekjoonHub

---
+++ 백준/Platinum/15678. 연세워터파크/연세워터파크.py
@@ -0,0 +1,22 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+
+N, D = map(int, input().split())
+dp = list(map(int, input().split()))
+
+qu = deque()
+qu.append(0)
+for i in range(1, N):
+    while qu[0] < i-D:
+        qu.popleft()
+    dp[i] += dp[qu[0]] if dp[qu[0]] > 0 else 0
+    while qu and dp[qu[-1]] < dp[i]:
+        qu.pop()
+    qu.append(i)
+
+print(max(dp))
+
+
+
