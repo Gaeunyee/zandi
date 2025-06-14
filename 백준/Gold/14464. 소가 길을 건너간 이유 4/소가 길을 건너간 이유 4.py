85f1d959db463815a0361a8adb869e3860202f5b
Gaeunyee <162317314+Gaeunyee@users.noreply.github.com>
2025-06-15 01:26:42 +0900

    [Gold I] Title: 소가 길을 건너간 이유 4, Time: 76 ms, Memory: 37728 KB -BaekjoonHub

--- 백준/Gold/14464. 소가 길을 건너간 이유 4/소가 길을 건너간 이유 4.py
+++ 백준/Gold/14464. 소가 길을 건너간 이유 4/소가 길을 건너간 이유 4.py
@@ -2,31 +2,33 @@ import sys
 from heapq import *
 input = sys.stdin.readline
 
-
-C, N = map(int, input().split())
-chicken = []
-for _ in range(C):
-    chicken.append(int(input().strip()))
-
-cow = []
-for _ in range(N):
-    cow.append(tuple(map(int, input().split())))
-
-cow.sort()
-chicken.sort()
-cow.append((10**9 + 7, 10**9 + 7))
-qu = []
-p = 0
-res = 0
-for i in chicken:
-    while cow[p][0] <= i:
-        heappush(qu, cow[p][1])
-        p += 1
-    while qu and qu[0] < i:
-        heappop(qu)
-    if qu:
-        heappop(qu)
-        res += 1
-
-print(res)
+def solve():
+    C, N = map(int, input().split())
+    chicken = []
+    for _ in range(C):
+        chicken.append(int(input().strip()))
+    
+    cow = []
+    for _ in range(N):
+        cow.append(tuple(map(int, input().split())))
+    
+    cow.sort()
+    chicken.sort()
+    cow.append((10**9 + 7, 10**9 + 7))
+    qu = []
+    p = 0
+    res = 0
+    for i in chicken:
+        while cow[p][0] <= i:
+            heappush(qu, cow[p][1])
+            p += 1
+        while qu and qu[0] < i:
+            heappop(qu)
+        if qu:
+            heappop(qu)
+            res += 1
+    
+    print(res)
+    
+solve()
 
